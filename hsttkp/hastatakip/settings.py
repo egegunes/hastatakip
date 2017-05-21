38971de73b2fafe603b7960d258e79b0c389fae5
Ege Güneş <egegunes@gmail.com>
2017-05-21 22:40:44 +0300

    Revert "Migrate to postgresql database"
    This reverts commit 1daea4e2de7c460813922a64a62ad808d42bbafa.

--- hsttkp/hastatakip/settings.py
+++ hsttkp/hastatakip/settings.py
@@ -66,12 +66,8 @@ WSGI_APPLICATION = 'hastatakip.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hastatakip',
-        'USER': 'hastatakip',
-        'PASSWORD': 'hastatakip',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db/db.sqlite3'),
     },
 }
 
