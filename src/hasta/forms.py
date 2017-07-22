d19fd594e3103d4b4d2b0109e2ad63ab22c6b179
Ege Güneş <egegunes@gmail.com>
2017-07-23 00:38:22 +0300

    Make hasta.forms PEP8 compatible

--- src/hasta/forms.py
+++ src/hasta/forms.py
@@ -1,17 +1,14 @@
 # -*- coding: utf-8 -*-
 
-import datetime
 import itertools
 
-from django.utils.translation   import ugettext_lazy as _
-from django.utils.text          import slugify
-from django.utils               import timezone
-from django                     import forms
+from django.utils.translation import ugettext_lazy as _
+from django.utils.text import slugify
+from django import forms
 
-from dal                        import autocomplete
+from hasta.models import Hasta
+from hasta.fields import ListTextWidget
 
-from hasta.models               import Hasta
-from hasta.fields               import ListTextWidget
 
 class HastaCreateForm(forms.ModelForm):
     SIGORTA = (
@@ -23,142 +20,140 @@ class HastaCreateForm(forms.ModelForm):
         'AKSIGORTA',
         'ACIBADEM',
         'CHECKUP')
-    KADIN = 'K'
-    ERKEK = 'E'
     CINSIYET = (
         (' ', ' '),
-        (KADIN, 'Kadın'),
-        (ERKEK, 'Erkek')
-    ) 
+        ('K', 'Kadın'),
+        ('E', 'Erkek')
+    )
     ad = forms.CharField(
-        required = True,
-        max_length = 30,
-        widget = forms.widgets.TextInput(attrs = {'class': 'form-control'}),
-        label = _('Ad')
+        required=True,
+        max_length=30,
+        widget=forms.widgets.TextInput(attrs={'class': 'form-control'}),
+        label=_('Ad')
     )
     soyad = forms.CharField(
-        required = True,
-        max_length = 30,
-        widget = forms.widgets.TextInput(attrs = {'class': 'form-control'}),
-        label = _('Soyad')
+        required=True,
+        max_length=30,
+        widget=forms.widgets.TextInput(attrs={'class': 'form-control'}),
+        label=_('Soyad')
     )
     tc_kimlik_no = forms.CharField(
-        required = False,
-        max_length = 11,
-        widget = forms.widgets.TextInput(attrs = {'class': 'form-control'}),
-        label = _('TC Kimlik No')
+        required=False,
+        max_length=11,
+        widget=forms.widgets.TextInput(attrs={'class': 'form-control'}),
+        label=_('TC Kimlik No')
     )
     cinsiyet = forms.ChoiceField(
-        required = True,
-        widget = forms.widgets.Select(attrs = {'class': 'form-control'}),
-        choices = CINSIYET,
-        label = _('Cinsiyet')
+        required=True,
+        widget=forms.widgets.Select(attrs={'class': 'form-control'}),
+        choices=CINSIYET,
+        label=_('Cinsiyet')
     )
     dogum_tarihi = forms.DateField(
-        required = True,
-        widget = forms.widgets.DateInput(attrs = {'class':'form-control datepicker'}),
-        label = _('Doğum tarihi')
+        required=True,
+        widget=forms.widgets.DateInput(attrs={'class': 'form-control datepicker'}),
+        label=_('Doğum tarihi')
     )
     meslek = forms.CharField(
-        required = False,
-        max_length = 30,
-        widget = forms.widgets.TextInput(attrs = {'class': 'form-control'}),
-        label = _('Meslek')
+        required=False,
+        max_length=30,
+        widget=forms.widgets.TextInput(attrs={'class': 'form-control'}),
+        label=_('Meslek')
     )
     es_ad = forms.CharField(
-        required = False,
-        max_length = 30,
-        widget = forms.widgets.TextInput(attrs = {'class': 'form-control'}),
-        label = _('Eş ad')
+        required=False,
+        max_length=30,
+        widget=forms.widgets.TextInput(attrs={'class': 'form-control'}),
+        label=_('Eş ad')
     )
     es_meslek = forms.CharField(
-        required = False,
-        max_length = 30,
-        widget = forms.widgets.TextInput(attrs = {'class': 'form-control'}),
-        label = _('Eş meslek')
+        required=False,
+        max_length=30,
+        widget=forms.widgets.TextInput(attrs={'class': 'form-control'}),
+        label=_('Eş meslek')
     )
     cep_telefon = forms.CharField(
-        required = False,
-        max_length = 13,
-        widget = forms.widgets.TextInput(attrs = {'class': 'form-control'}),
-        label = _('Cep telefon')
+        required=False,
+        max_length=13,
+        widget=forms.widgets.TextInput(attrs={'class': 'form-control'}),
+        label=_('Cep telefon')
     )
     ev_telefon = forms.CharField(
-        required = False,
-        max_length = 13,
-        widget = forms.widgets.TextInput(attrs = {'class': 'form-control'}),
-        label = _('Ev telefon')
+        required=False,
+        max_length=13,
+        widget=forms.widgets.TextInput(attrs={'class': 'form-control'}),
+        label=_('Ev telefon')
     )
     is_telefon = forms.CharField(
-        required = False,
-        max_length = 13,
-        widget = forms.widgets.TextInput(attrs = {'class': 'form-control'}), 
-        label = _('İş telefon')
+        required=False,
+        max_length=13,
+        widget=forms.widgets.TextInput(attrs={'class': 'form-control'}),
+        label=_('İş telefon')
     )
     adres = forms.CharField(
-        required = False,
-        max_length = 255,
-        widget = forms.widgets.TextInput(attrs = {'class': 'form-control'}),
-        label = _('Adres')
+        required=False,
+        max_length=255,
+        widget=forms.widgets.TextInput(attrs={'class': 'form-control'}),
+        label=_('Adres')
     )
     semt = forms.CharField(
-        required = False,
-        max_length = 30,
-        widget = forms.widgets.TextInput(attrs = {'class': 'form-control'}),
-        label = _('Semt')
+        required=False,
+        max_length=30,
+        widget=forms.widgets.TextInput(attrs={'class': 'form-control'}),
+        label=_('Semt')
     )
     eposta = forms.CharField(
-        required = False,
-        max_length = 254,
-        widget = forms.widgets.EmailInput(attrs = {'class': 'form-control'}),
-        label = _('Eposta Adresi')
+        required=False,
+        max_length=254,
+        widget=forms.widgets.EmailInput(attrs={'class': 'form-control'}),
+        label=_('Eposta Adresi')
     )
     sigorta = forms.CharField(
-        required = False,
-        widget = ListTextWidget(choices=SIGORTA, name='sigorta_list', attrs = {'class': 'form-control'}),
-        label = _('Sigorta')
+        required=False,
+        widget=ListTextWidget(choices=SIGORTA, name='sigorta_list', attrs={'class': 'form-control'}),
+        label=_('Sigorta')
     )
     cocuklar = forms.CharField(
-        required = False,
-        max_length = 30,
-        widget = forms.widgets.TextInput(attrs = {'class': 'form-control'}),
-        label = _('Çocuklar')
+        required=False,
+        max_length=30,
+        widget=forms.widgets.TextInput(attrs={'class': 'form-control'}),
+        label=_('Çocuklar')
     )
     soygecmis = forms.CharField(
-        required = False,
-        max_length = 255,
-        widget = forms.widgets.TextInput(attrs = {'class': 'form-control'}),
-        label = _('Soygeçmiş')
+        required=False,
+        max_length=255,
+        widget=forms.widgets.TextInput(attrs={'class': 'form-control'}),
+        label=_('Soygeçmiş')
     )
     ozgecmis = forms.CharField(
-        required = False,
-        max_length = 255,
-        widget = forms.widgets.TextInput(attrs = {'class': 'form-control'}),
-        label = _('Özgeçmiş')
+        required=False,
+        max_length=255,
+        widget=forms.widgets.TextInput(attrs={'class': 'form-control'}),
+        label=_('Özgeçmiş')
     )
     sigara = forms.CharField(
-        required = False,
-        max_length = 30,
-        widget = forms.widgets.TextInput(attrs = {'class': 'form-control'}),
-        label = _('Sigara')
+        required=False,
+        max_length=30,
+        widget=forms.widgets.TextInput(attrs={'class': 'form-control'}),
+        label=_('Sigara')
     )
     alkol = forms.CharField(
-        required = False,
-        max_length = 30,
-        widget = forms.widgets.TextInput(attrs = {'class': 'form-control'}),
-        label = _('Alkol')
+        required=False,
+        max_length=30,
+        widget=forms.widgets.TextInput(attrs={'class': 'form-control'}),
+        label=_('Alkol')
     )
     boy = forms.IntegerField(
-        required = False,
-        widget = forms.widgets.NumberInput(attrs = {'class': 'form-control'}),
-        initial = 0,
-        label = _('Boy')
+        required=False,
+        widget=forms.widgets.NumberInput(attrs={'class': 'form-control'}),
+        initial=0,
+        label=_('Boy')
     )
     kilo = forms.IntegerField(
-        required = False,
-        widget = forms.widgets.NumberInput(attrs = {'class': 'form-control'}),
-        initial = 0,
-        label = _('Kilo')
+        required=False,
+        widget=forms.widgets.NumberInput(attrs={'class': 'form-control'}),
+        initial=0,
+        label=_('Kilo')
     )
 
     class Meta:
@@ -175,7 +170,7 @@ class HastaCreateForm(forms.ModelForm):
             if hasta.ahsevk_done:
                 instance.ahsevk_done = True
             return super(HastaCreateForm, self).save()
-            
+
         instance.slug = orig = slugify(instance.ad + " " + instance.soyad)
 
         for x in itertools.count(1):
