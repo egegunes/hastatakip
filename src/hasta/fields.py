03cd524c55aa2a3df4e88f754be13bbdd7fe1cbe
Ege Güneş <egegunes@gmail.com>
2017-07-24 22:21:56 +0300

    Make hasta.fields PEP8 compatible

--- src/hasta/fields.py
+++ src/hasta/fields.py
@@ -1,5 +1,6 @@
 from django import forms
 
+
 class ListTextWidget(forms.TextInput):
     def __init__(self, choices, name, *args, **kwargs):
         super(ListTextWidget, self).__init__(*args, **kwargs)
