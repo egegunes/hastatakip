f6ecbfc1ecd16eff125101a413e5209a6dd3321e
Ege Güneş <egegunes@gmail.com>
2018-10-22 22:09:01 +0300

    Don't activate virtualenv before restore

--- bin/restore.sh
+++ bin/restore.sh
@@ -2,12 +2,11 @@
 
 set -eu
 
-source "$HOME/environment/bin/activate"
 source "$HOME/hastatakip/playbooks/env/backup"
 
 echo "Initiated restore at $(date)"
 
-ansible-playbook \
+$HOME/environment/bin/ansible-playbook \
     -c local \
     -i "localhost," \
     "$HOME/hastatakip/playbooks/restore.yml"
