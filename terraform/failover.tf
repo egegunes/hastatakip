174639d53c67461943301e5885a639ced81d7a59
Ege Güneş <egegunes@gmail.com>
2018-12-02 15:18:34 +0300

    Automatic failover with Terraform

---
+++ terraform/failover.tf
@@ -0,0 +1,35 @@
+variable "do_token" {}
+variable "ssh_password" {}
+
+provider "digitalocean" {
+    token = "${var.do_token}"
+}
+
+resource "digitalocean_droplet" "hastatakip" {
+    image    = "40921535"
+    name     = "hastatakip-1"
+    region   = "fra1"
+    size     = "s-1vcpu-2gb"
+    ssh_keys = [16992631]
+
+    provisioner "remote-exec" {
+        inline = [
+            "sudo certbot renew",
+            "sudo su - hastatakip -c '/home/hastatakip/hastatakip/bin/restore.sh'",
+        ]
+
+        connection {
+            type = "ssh"
+            user = "egegunes"
+            password = "${var.ssh_password}"
+        }
+    }
+}
+
+resource "digitalocean_record" "hastatakip" {
+    domain = "zygns.com"
+    type   = "A"
+    name   = "hastatakip"
+    ttl    = 300
+    value  = "${digitalocean_droplet.hastatakip.ipv4_address}"
+}
